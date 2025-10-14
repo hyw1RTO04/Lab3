34c3be300b19a5d64027eb85e69f4413cf5d8eb3
user <user@WIN-N7P1J9L4V2S>
2025-10-14 23:34:40 +0300

    Изменил метод Punctuation

--- Punctuation.cs
+++ Punctuation.cs
@@ -2,6 +2,13 @@ namespace TextTokenizerApp
 {
     public class Punctuation : Token
     {
-        public Punctuation(string value) : base(value) { }
+        public Punctuation(string value) : base(Validate(value)) { }
+
+        private static string Validate(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("«начение знака препинани€ не может быть пустым.", nameof(value));
+            return value;
+        }
     }
 }
