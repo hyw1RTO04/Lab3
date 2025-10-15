0b2d887e517ccb95ebd0f3974aec182f92fb097b
user <user@WIN-N7P1J9L4V2S>
2025-10-15 13:58:55 +0300

    Добавил атрибуты для сериализации

--- Token.cs
+++ Token.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Xml.Serialization;
 
 namespace TextTokenizerApp
 {
-    
+    [XmlInclude(typeof(Word))]
+    [XmlInclude(typeof(Punctuation))]
     public abstract class Token
     {
+        [XmlElement("Value")]
         public string Value { get; protected set; }
+
+        protected Token() { }
 
         protected Token(string value)
         {
