0ff1f7153951b6148eade1a88beaab1a725acbaa
user <user@WIN-N7P1J9L4V2S>
2025-10-15 14:01:01 +0300

    Добавил атрибуты для сериализации

--- Word.cs
+++ Word.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Xml.Serialization;
 
 namespace TextTokenizerApp
 {
     public class Word : Token
     {
+        public Word() : base() { }
+
         public Word(string value) : base(value) { }
 
+        [XmlIgnore]
         public int Length => Value.Length;
 
-        
         public bool StartsWithConsonant()
         {
             const string consonants = "бвгджзйклмнпрстфхцчшщbcdfghjklmnpqrstvwxyz";
