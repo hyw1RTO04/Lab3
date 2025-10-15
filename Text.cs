b865332fa0f009fa6357332e0317ef8f8cdd9100
user <user@WIN-N7P1J9L4V2S>
2025-10-15 14:06:04 +0300

    Добавил атрибуты к сериализации

--- Text.cs
+++ Text.cs
@@ -1,27 +1,27 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace TextTokenizerApp
 {
-    
+    [XmlRoot("Text")]
     public class Text
     {
+        [XmlArray("Sentences")]
+        [XmlArrayItem("Sentence")]
         private readonly List<Sentence> _sentences = new();
 
+        [XmlIgnore]
         public IReadOnlyList<Sentence> Sentences => _sentences;
 
-        public void AddSentence(Sentence sentence)
-        {
-            _sentences.Add(sentence);
-        }
+        public void AddSentence(Sentence sentence) => _sentences.Add(sentence);
 
         public override string ToString()
         {
             var sb = new StringBuilder();
             foreach (var s in _sentences)
-            {
                 sb.AppendLine(s.ToString());
-            }
             return sb.ToString();
         }
     }
